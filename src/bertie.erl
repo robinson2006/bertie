1ce37b220e76ffdf3de6df701e5f65a6bff7e51c
Leo <leo@leoerl.local>
2015-08-02 13:52:15 +0800

    t

---
+++ src/bertie.erl
@@ -0,0 +1,17 @@
+%%%-------------------------------------------------------------------
+%%% @author leo
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 02. 八月 2015 下午1:41
+%%%-------------------------------------------------------------------
+-module(bertie).
+-author("leo").
+
+%% API
+-export([start/0]).
+
+start() ->
+  io:format("Hello my name is Bertie~n").
+
